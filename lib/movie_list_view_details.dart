852cdf03b9115b8a0eceac3cd12b85a07cec083c
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-07-07 22:34:49 -0500

    Update movie_list_view_details.dart

--- lib/movie_list_view_details.dart
+++ lib/movie_list_view_details.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:movie_app/movie.dart';
+import 'package:movie_app/movie.dart'
 
 class MovieListViewDetails extends StatelessWidget {
   final String movieName;
